194b1d5e36ce9f05405fd858d1c765ee021ea5bc
Steve Forde <sforde08@gmail.com>
2025-11-10 10:27:04 +0000

    FIXED: changing logic stucture for payment cancelled

--- server.js
+++ server.js
@@ -503,7 +503,7 @@ app.get("/get-session-email/:sessionId", async (req, res) => {
 // 🌐 MIDDLEWARE
 // ===================================================
 
-// ===================================================
+// =default================================================
 // 💳 STRIPE CHECKOUT SESSION (FINAL, static domain)
 // ===================================================
 app.post("/create-checkout-session", async (req, res) => {
@@ -514,8 +514,11 @@ app.post("/create-checkout-session", async (req, res) => {
       mode: "payment",
       line_items: [{ price: process.env.PRICE_ID, quantity: 1 }],
 
+      // === THIS IS THE NEW LINE ===
+      automatic_payment_methods: { enabled: true },
+      // ============================
+
       success_url: "https://quickcoverletter-static.onrender.com/?session_id={CHECKOUT_SESSION_ID}",
-      // In /create-checkout-session route
       cancel_url: "https://quickcoverletter-backend.onrender.com/cancel/{CHECKOUT_SESSION_ID}",
 
       customer_email: email || undefined,
